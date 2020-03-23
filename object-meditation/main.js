bdb6d5cb8ab7c4946077c4fc28264ed8b70facf8
Alisha Dhir <alishadhir@Second-Gina.fios-router.home>
2020-03-22 21:50:11 -0400

    almost done woohoo

--- object-meditation/main.js
+++ object-meditation/main.js
@@ -1,9 +1,18 @@
-$("div").on( "click", "#milk", function( event ) {
-  $(event.delegateTarget ).css( "background-color", "#FFFFFF");
-});
 
-$("div").on( "click", "#accurate", function( event ) {
-  $(event.delegateTarget ).css( "background-color", "#bdc6ef");
+$(document).ready(function() {
+
+  $("#normalbutton").on("click", function( event ) {
+    $(document.body).css( "background-color", "#f9f4e5");
+  });
+
+  $("#discobutton").on("click", function( event ) {
+    $(document.body).css( "background-image", "repeating-linear-gradient(red, yellow, blue)");
+  });
+
+  $("#chillbutton").on("click", function( event ) {
+    $(document.body).css( "background-color", "#b1dfe5");
+  });
+
 });
 
 // https://www.w3resource.com/jquery-exercises/jquery-events-exercise-8.php
---
+++ object-meditation/mainegg.js
@@ -0,0 +1,7 @@
+// MAKE THE SCREEN FILL WITH MILK
+$(document).ready(function() {
+
+// $("#thebox").css("background-color", "#fff");
+ $("#thebox").animate({height:"100%"}, {duration: 20000});
+
+});
